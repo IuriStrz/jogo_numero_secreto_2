b8ad21691cf246ea31df22b15dfd6dac4e9e4fd9
IuriStrz <iuristr@gmail.com>
2025-11-24 17:43:49 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,39 @@
+function sortear () {
+    let Quantidade = parseInt(document.getElementById("quantidade").value);
+    let NumeroDe = parseInt(document.getElementById("de").value);
+    let NumeroAte = parseInt(document.getElementById("ate").value);
+    let Contador = 1;
+    let ListaNumeros = [];
+
+    //alert("Quantidade: " + Quantidade + ", N° de: " + NumeroDe + ", N° até: " + NumeroAte);
+
+    if (Quantidade > NumeroAte - NumeroDe) {
+        alert("a quantidade de números sorteados precisa ser menor ou igual ao número de opções disponíveis entre o número de e até")
+        process.exit();
+    }
+
+    while (Contador <= Quantidade) {
+        let Numero = ObterNumeroAleatorio(NumeroDe, NumeroAte);
+        while (ListaNumeros.includes(Numero) == true) {
+            Numero = ObterNumeroAleatorio(NumeroDe, NumeroAte)
+        }
+        ListaNumeros.push(Numero);
+        ++Contador;
+    }
+
+    //console.log(ListaNumeros);
+    let Resultado = document.getElementById ("resultado");
+    Resultado.innerHTML = '<label class="texto__paragrafo">Números sorteados:' + ListaNumeros + '</label>'
+
+}
+
+function ObterNumeroAleatorio (A, B) {
+  const min = Math.ceil(Math.min(A, B));
+  const max = Math.floor(Math.max(A, B));
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function reiniciar () {
+
+}
